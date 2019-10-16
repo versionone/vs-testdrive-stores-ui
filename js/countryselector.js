2ff84691362115deebad686ba9e63086490cab52
Shannon Cruey <shannon@cruey.net>
2019-10-16 08:04:40 -0400

    Create countryselector.js

---
+++ js/countryselector.js
@@ -0,0 +1,4 @@
+  var citiesAU =  ["Sydney", "Melbourne", "Brisbane", "Perth"],
+      citiesUS =  ["New York", "Miami", "San Fransisco", "Los Angeles"],
+      cities = citiesAU.concat(citiesUS);
+  document.getElementById("msg").innerHTML = cities;
